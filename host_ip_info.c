751be48ced97969ae40db2ec81d5b72e888c714d
drew york <119648808+dre-yo@users.noreply.github.com>
2024-07-25 15:48:08 -0700

    changed format

--- host_ip_info.c
+++ host_ip_info.c
@@ -2,35 +2,31 @@
 Andrew York
 Program that finds ip address of current host
 */
+#include <arpa/inet.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <netdb.h>
 #include <string.h>
-#include <arpa/inet.h>
+#include <unistd.h>
 
-int main()
-{
+int main() {
   char hostname[1024];
   struct hostent *host_info;
   struct in_addr **addr_list;
 
-  if (gethostname(hostname, sizeof(hostname)) < 0)
-  {
+  if (gethostname(hostname, sizeof(hostname)) < 0) {
     perror("gethostname");
     exit(1);
   }
 
-  if ((host_info = gethostbyname(hostname)) == NULL)
-  {
+  if ((host_info = gethostbyname(hostname)) == NULL) {
     herror("gethostbyname");
     exit(1);
   }
 
   addr_list = (struct in_addr **)host_info->h_addr_list;
   printf("IP adresses for %s:\n", hostname);
-  for (int i = 0; addr_list[i] != NULL; i++)
-  {
+  for (int i = 0; addr_list[i] != NULL; i++) {
     printf("%s\n", inet_ntoa(*addr_list[i]));
   }
   return 0;
