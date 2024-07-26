c2f9a62e1688a4be6af344edabe1afba5e509e45
drew york <119648808+dre-yo@users.noreply.github.com>
2024-07-25 17:57:08 -0700

    added file to work on ip6 stuff

---
+++ ip6_system_calls.c
@@ -0,0 +1,14 @@
+/*
+Andrew York
+Program that finds ip6 address info
+*/
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main() { return 0; }
