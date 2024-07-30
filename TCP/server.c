c3db33b3b91971af9706fd07ac44cd50078b7dfc
drew york <119648808+dre-yo@users.noreply.github.com>
2024-07-29 19:11:58 -0700

    added helper functions

--- TCP/server.c
+++ TCP/server.c
@@ -1,8 +1,3 @@
-/*
-simple server program
-https://beej.us/guide/bgnet/html/split/client-server-background.html
-*/
-
 #include <arpa/inet.h>
 #include <errno.h>
 #include <netdb.h>
@@ -19,6 +14,7 @@ https://beej.us/guide/bgnet/html/split/client-server-background.html
 #define PORT "9090"
 #define BACKLOG 5
 
+// Signal handler for reaping zombie processes
 void sigchld_handler(int s) {
   int saved_errno = errno;
   while (waitpid(-1, NULL, WNOHANG) > 0)
@@ -26,33 +22,30 @@ void sigchld_handler(int s) {
   errno = saved_errno;
 }
 
+// Get the sockaddr, IPv4 or IPv6
 void *get_in_addr(struct sockaddr *sa) {
   if (sa->sa_family == AF_INET) {
     return &(((struct sockaddr_in *)sa)->sin_addr);
   }
-
   return &(((struct sockaddr_in6 *)sa)->sin6_addr);
 }
 
-int main(void) {
-  int sockfd, new_fd;
-  struct addrinfo hints, *servinfo, *p;
-  struct sockaddr_storage their_addr;
-  socklen_t sin_size;
+// Set up the SIGCHLD handler to reap dead child processes
+void setup_sigchld_handler() {
   struct sigaction sa;
-  int yes = 1;
-  char s[INET6_ADDRSTRLEN];
-  int rv;
-
-  memset(&hints, 0, sizeof hints);
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = AI_PASSIVE;
-
-  if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
-    fprintf(stderr, "getaddrinfo %s\n", gai_strerror(rv));
-    return 1;
+  sa.sa_handler = sigchld_handler;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART;
+  if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+    perror("sigaction");
+    exit(1);
   }
+}
+
+// Create a socket and bind it to the specified port
+int create_and_bind_socket(struct addrinfo *servinfo) {
+  struct addrinfo *p;
+  int sockfd, yes = 1;
 
   for (p = servinfo; p != NULL; p = p->ai_next) {
     if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
@@ -64,32 +57,67 @@ int main(void) {
       perror("setsockopt");
       exit(1);
     }
+
     if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
       close(sockfd);
       perror("server: bind");
       continue;
     }
+
     break;
   }
-
-  freeaddrinfo(servinfo);
 
   if (p == NULL) {
     fprintf(stderr, "server: failed to bind\n");
     exit(1);
   }
+
+  return sockfd;
+}
+
+// Handle a new connection
+void handle_connection(int new_fd, struct sockaddr_storage their_addr) {
+  char s[INET6_ADDRSTRLEN];
+  inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr),
+            s, sizeof s);
+  printf("server: got connection from %s\n", s);
+
+  if (!fork()) {
+    if (send(new_fd, "Hello, world!", 13, 0) == -1) {
+      perror("send");
+    }
+    close(new_fd);
+    exit(0);
+  }
+  close(new_fd);
+}
+
+int main(void) {
+  int sockfd, new_fd;
+  struct addrinfo hints, *servinfo;
+  struct sockaddr_storage their_addr;
+  socklen_t sin_size;
+  int rv;
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return 1;
+  }
+
+  sockfd = create_and_bind_socket(servinfo);
+  freeaddrinfo(servinfo);
+
   if (listen(sockfd, BACKLOG) == -1) {
     perror("listen");
     exit(1);
   }
 
-  sa.sa_handler = sigchld_handler;
-  sigemptyset(&sa.sa_mask);
-  sa.sa_flags = SA_RESTART;
-  if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-    perror("sigaction");
-    exit(1);
-  }
+  setup_sigchld_handler();
 
   printf("server: waiting for connections...\n");
 
@@ -101,19 +129,14 @@ int main(void) {
       continue;
     }
 
-    inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr),
-              s, sizeof s);
-    printf("server: got connection from %s\n", s);
-
-    if (!fork()) {
-      close(sockfd);
-      if (send(new_fd, "Hello, world!", 13, 0) == -1) {
-        perror("send");
-      }
-      close(new_fd);
+    if (!fork()) {   // This is the child process
+      close(sockfd); // Child doesn't need the listener
+      handle_connection(new_fd, their_addr);
       exit(0);
     }
-    close(new_fd);
+
+    close(new_fd); // Parent doesn't need this
   }
+
   return 0;
 }
